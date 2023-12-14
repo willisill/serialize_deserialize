d583bbb79956ff0361d9c83864f8e2d772c87003
Samuel <187swachira@gmail.com>
2023-12-13 19:42:38 -0500

    completed serialize

--- server.js
+++ server.js
@@ -10,10 +10,13 @@ const server = http.createServer((req, res) => {
 
   req.on("end", () => {
     // Parse the body of the request as JSON if Content-Type header is
-      // application/json
+    // application/json
     // Parse the body of the request as x-www-form-urlencoded if Content-Type
-      // header is x-www-form-urlencoded
-    if (reqBody) {
+    // header is x-www-form-urlencoded
+
+    if (req.headers['content-type'] === 'application/json') {
+      req.body = JSON.parse(reqBody)
+    } else if (req.headers['content-type'] === 'x-www-form-urlencoded.') {
       req.body = reqBody
         .split("&")
         .map((keyValuePair) => keyValuePair.split("="))
@@ -23,14 +26,20 @@ const server = http.createServer((req, res) => {
           acc[key] = value;
           return acc;
         }, {});
-
-      // Log the body of the request to the terminal
-      console.log(req.body);
     }
+
+
+    // Log the body of the request to the terminal
+    console.log(req.body);
+
 
     const resBody = {
       "Hello": "World!"
     };
+
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify(resBody))
 
     // Return the `resBody` object as JSON in the body of the response
   });
